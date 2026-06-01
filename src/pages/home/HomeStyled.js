de88b2276bd571a16a29bc646d66aae94f0e2618
solange <solangeleitesg@gmail.com>
2026-05-31 21:50:48 -0300

    probando background desde url clou

--- src/pages/home/HomeStyled.js
+++ src/pages/home/HomeStyled.js
@@ -10,7 +10,7 @@ export const HomeContainer = styled.div`
   min-height: 100dvh;
   background-size: auto;
   background-position: center;
-  background-image: url('/src/assets/noisy-texture-200x200-o10-d13-c-f3eee7-t0.png');
+  background-image: url('https://res.cloudinary.com/dgyubvlec/image/upload/v1779151199/noisy-texture-200x200-o10-d13-c-f3eee7-t0_nnprzd.png');
   z-index: 300;
 
 `;
